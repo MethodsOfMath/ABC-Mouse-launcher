bf094057555615cfa9d9fea01fc15acc606219b0
Scottt <scott.geyer@eagles.usm.edu>
2017-05-13 16:50:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to provide a one line install for a Sugarizer launcher for the Raspberry Pi.
+# Script to provide a one line install for a ABC Mouse launcher for the Raspberry Pi.
 # 
 # Motivating use case:
 # - To be able to provide consise and robust instructions for installing a basic
@@ -8,10 +8,10 @@
 #   
 # Usage:
 # 
-# curl -L https://github.com/punkbass/sugarizer-launcher/raw/master/install.sh | sh
-# wget --no-delete-certificate https://github.com/punkbass/sugarizer-launcher/raw/master/install.sh -O - | sh
+# curl -L https://github.com/MethodsOfMath/abc-mouse-launcher/raw/master/install.sh | sh
+# wget --no-delete-certificate https://github.com/MethodsOfMath/abc-mouse-launcher/raw/master/install.sh -O - | sh
 
-LAUNCHER_DIR="/opt/kano/sugarizer-launcher"
+LAUNCHER_DIR="/opt/kano/abc-mouse-launcher"
 
 delete()
 {  
@@ -37,31 +37,31 @@ sudo apt-get update
 
 if test -d $LAUNCHER_DIR;
 then
-  	echo  -e "\n[Sugarizer Launcher installer] It looks like you've already got the Sugarizer Launcher installed."
-  	echo  -e "\n[Sugarizer Launcher installer] You'll need to remove $LAUNCHER_DIR to install."
-  	echo  -e "\nWould you like to remove [Sugarizer Launcher installer] directory[Y/n]?"
+  	echo  -e "\n[ABC Mouse Launcher installer] It looks like you've already got the Sugarizer Launcher installed."
+  	echo  -e "\n[ABC Mouse Launcher installer] You'll need to remove $LAUNCHER_DIR to install."
+  	echo  -e "\nWould you like to remove [ABC Mouse Launcher installer] directory[Y/n]?"
 	delete $LAUNCHER_DIR ;
 fi
 
-echo -e "[Sugarizer Launcher installer] Installing Chromium ..."
+echo -e "[ABC Mouse Launcher installer] Installing Chromium ..."
 sudo apt-get -y install chromium-browser
 
 echo  -e "\nInstalling git ...\n"
 sudo apt-get -y install git
 
-echo -e "\n[Sugarizer Launcher installer] Installing desktop icon."
+echo -e "\n[ABC Mouse Launcher installer] Installing desktop icon."
 sudo mkdir -p $LAUNCHER_DIR
-sudo git clone git://github.com/punkbass/sugarizer-launcher.git $LAUNCHER_DIR
-sudo cp $LAUNCHER_DIR/sugarizer.desktop /usr/share/applications
+sudo git clone git://github.com/MethodsOfMath/sugarizer-launcher.git $LAUNCHER_DIR
+sudo cp $LAUNCHER_DIR/abc-mouse.desktop /usr/share/applications
 
-if test -e ~/Desktop/sugarizer.desktop; 
+if test -e ~/Desktop/abc-mouse.desktop; 
 then
-	echo -e "\n~/Desktop/sugarizer.desktop file has existes!\nDo you want to remove it[y/n]?"
-	delete ~/Desktop/sugarizer.desktop;	
+	echo -e "\n~/Desktop/abc-mouse.desktop file has existes!\nDo you want to remove it[y/n]?"
+	delete ~/Desktop/abc-mouse.desktop;	
 fi
 	
-sudo ln -s $LAUNCHER_DIR/sugarizer.desktop ~/Desktop/sugarizer.desktop
-sudo chmod +x ~/Desktop/sugarizer.desktop
-sudo chown $(whoami) ~/Desktop/sugarizer.desktop
+sudo ln -s $LAUNCHER_DIR/abc-mouse.desktop ~/Desktop/abc-mouse.desktop
+sudo chmod +x ~/Desktop/abc-mouse.desktop
+sudo chown $(whoami) ~/Desktop/abc-mouse.desktop
 sleep  2
 echo -e "\nComplete!\n"
